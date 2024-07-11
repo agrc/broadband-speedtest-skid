5e6b440f9ecb7da51f828959b860832bc85c4184
Jake Adams <jdadams@utah.gov>
2024-07-11 15:29:06 -0600

    chore: update requirements.txt, ruff fixes

--- tests/test_bb_speed.py
+++ tests/test_bb_speed.py
@@ -1,4 +1,3 @@
-import pytest
 import pandas as pd
 import pandas.testing as tm
 
@@ -6,32 +5,32 @@ from bb_speed import main, helpers
 
 
 def test_get_secrets_from_gcp_location(mocker):
-    mocker.patch('pathlib.Path.exists', return_value=True)
-    mocker.patch('pathlib.Path.read_text', return_value='{"foo":"bar"}')
+    mocker.patch("pathlib.Path.exists", return_value=True)
+    mocker.patch("pathlib.Path.read_text", return_value='{"foo":"bar"}')
 
     secrets = main._get_secrets()
 
-    assert secrets == {'foo': 'bar'}
+    assert secrets == {"foo": "bar"}
 
 
 def test_get_secrets_from_local_location(mocker):
     exists_mock = mocker.Mock(side_effect=[False, True])
-    mocker.patch('pathlib.Path.exists', new=exists_mock)
-    mocker.patch('pathlib.Path.read_text', return_value='{"foo":"bar"}')
+    mocker.patch("pathlib.Path.exists", new=exists_mock)
+    mocker.patch("pathlib.Path.read_text", return_value='{"foo":"bar"}')
 
     secrets = main._get_secrets()
 
-    assert secrets == {'foo': 'bar'}
+    assert secrets == {"foo": "bar"}
     assert exists_mock.call_count == 2
 
 
 def test_load_census_data(mocker):
-    requests_mock = mocker.patch('bb_speed.helpers.requests')
-    requests_mock.get.return_value.json.return_value = [['DP02_0001E', 'state', 'county'], ['87802', '49', '057']]
+    requests_mock = mocker.patch("bb_speed.helpers.requests")
+    requests_mock.get.return_value.json.return_value = [["DP02_0001E", "state", "county"], ["87802", "49", "057"]]
 
-    output_df = helpers.load_census_data('', '')
+    output_df = helpers.load_census_data("", "")
 
-    test_df = pd.DataFrame([['87802', 'Weber County']], columns=['total_households', 'name'])
+    test_df = pd.DataFrame([["87802", "Weber County"]], columns=["total_households", "name"])
 
     tm.assert_frame_equal(test_df, output_df, check_names=False)
 
@@ -39,22 +38,25 @@ def test_load_census_data(mocker):
 def test_calc_county_summary(mocker):
 
     # households_df = pd.DataFrame([['10', 'Weber County'], ['20', 'Utah County']], columns=['total_households', 'name'])
-    households_df = pd.DataFrame({
-        'total_households': [10, 20],
-        'name': ['Weber County', 'Utah County'],
-    })
-    speedtests_df = pd.DataFrame({
-        'county': ['Weber County', 'Weber County', 'Utah County', 'Utah County'],
-        'id': [1, 1, 1, 1]
-    })
+    households_df = pd.DataFrame(
+        {
+            "total_households": [10, 20],
+            "name": ["Weber County", "Utah County"],
+        }
+    )
+    speedtests_df = pd.DataFrame(
+        {"county": ["Weber County", "Weber County", "Utah County", "Utah County"], "id": [1, 1, 1, 1]}
+    )
 
     summary_df = helpers.calc_county_summary(households_df, speedtests_df)
 
-    test_df = pd.DataFrame({
-        'tests': [2, 2],
-        'total_households': [20, 10],
-        'name': ['Utah County', 'Weber County'],
-        'percent_response': [.1, .2],
-    })
+    test_df = pd.DataFrame(
+        {
+            "tests": [2, 2],
+            "total_households": [20, 10],
+            "name": ["Utah County", "Weber County"],
+            "percent_response": [0.1, 0.2],
+        }
+    )
 
     tm.assert_frame_equal(test_df, summary_df, check_dtype=False)
